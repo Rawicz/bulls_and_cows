4dbbfac2a7e3fcc42399ed6b45b6e5c8af1e8530
Kurt Rawicz <kurt.ravich@gmail.com>
2021-03-12 22:05:31 +0300

    second version of RowAdapter

--- app/src/main/java/com/example/bullscows/RowsAdapter.java
+++ app/src/main/java/com/example/bullscows/RowsAdapter.java
@@ -8,13 +8,17 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
+// TODO: actually, I think it'd be better to leave bulls and cows handling to getView method only
+// TODO: and clear all excessive overridings
 public class RowsAdapter extends ArrayAdapter<String> {
    private ArrayList<Integer> bullsList = new ArrayList<Integer>();
    private ArrayList<Integer> cowsList = new ArrayList<Integer>();
+   private String keyword;
    private int count = 0;
 
-   public RowsAdapter(@NonNull Context context) {
+   public RowsAdapter(@NonNull Context context, String keyword) {
       super(context, R.layout.row, R.id.value);
+      this.keyword = keyword;
    }
 
    @Override
@@ -49,5 +53,35 @@ public class RowsAdapter extends ArrayAdapter<String> {
       setNotifyOnChange(false);
       super.add(object);
       count++;
+   }
+
+   public void renew(String keyword) {
+      clear();
+      this.keyword = keyword;
+   }
+
+   // TODO: check how findBulls and findCows methods work in the terminal
+   public int findBulls(String object) {
+      int bulls = 0;
+      for (int i = 0; i < 4; i++) {
+         if (object.charAt(i) == keyword.charAt(i)) bulls++;
+      }
+      return bulls;
+   }
+
+   public int findCows(String object) {
+      int cows = 0;
+      char[] keywordCopy = keyword.toCharArray();
+      char shot;
+      for (int i = 0; i < 4; i++) {
+         shot = object.charAt(i);
+         for (int j = 0; j < 4; j++) {
+            if (j != i && keywordCopy[j] == shot){
+               cows++;
+               keywordCopy[j] = '*';
+            }
+         }
+      }
+      return cows;
    }
 }
